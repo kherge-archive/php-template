ac71ba04ca7189c5710f6fe518d80775cbaf76d9
Kevin Herrera <kevin@herrera.io>
2013-06-18 14:47:47 -0700

    Removing unused dependencies.

--- res/templates/list.php
+++
@@ -1,6 +0,0 @@
-<?php use Herrera\Template\Engine; ?>
-<ul>
-<?php foreach (Engine::get($tpl['items'], array()) as $item): ?>
-  <li><?php echo htmlentities($item); ?></li>
-<?php endforeach; ?>
-</ul>
